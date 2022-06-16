7f792e01735c9344c5a5450b65fe739060defed1
Mkk818 <miriks818@gmail.com>
2022-06-16 22:31:52 +0900

    ニュース記事の大枠作成

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>News App</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: 'https://picsum.photos/id/866/200/200' }}
+          />
+        </View>{/* {styles.leftContainer}> */}
+        <View style={styles.rightContainer}>
+          <View>
+            <Text numberOfLines={3} style={styles.text}>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores
+              officiis necessitatibus tempore ipsa nemo nostrum beatae molestias
+              labore eos praesentium libero architecto deserunt, maxime, odit
+              perferendis, explicabo doloribus aperiam laboriosam.
+            </Text>
+            <Text style={styles.subText}>ReactNews</Text>
+          </View>
+        </View>{/* {styles.rightContainer}> */}
+      </View>{/* {styles.itemContainer}> */}
+    </View>// {styles.container}>
   );
 }
 
@@ -16,5 +33,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: '#555',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
   },
 });
