67776530ad51bc2fdffc77efcb9ba2cee077c552
Mkk818 <miriks818@gmail.com>
2022-06-18 12:36:36 +0900

    画面遷移(App.jsで描画していたものの切り分け) ・Home画面をHomeScreen.jsに移動 ・App.jsのreturnの中身をAppNavigator.jsに移動

--- App.js
+++ App.js
@@ -1,48 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{ useEffect, useState } from 'react';
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
-import ListItem from './components/ListItem';
-import Constants from 'expo-constants';
-import dummyArticles from './dummies/articles.json';
-import axios from 'axios';
-
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`;
+import React from 'react';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
-  const [articles, setArticles] = useState([]);
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
-  const fetchArticles = async () => {
-      try {
-        const response = await axios.get(URL);
-        setArticles(response.data.articles);
-      } catch (error) {
-        console.error(error);
-      }
-  }
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </SafeAreaView> // {styles.container}>
-  );
+  return <AppNavigator />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
