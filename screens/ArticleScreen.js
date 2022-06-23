8296dcbf3a6189802cc12261ffacb296422f2124
Mkk818 <miriks818@gmail.com>
2022-06-23 16:00:59 +0900

    dispatch

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,7 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -9,10 +12,24 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ArticleScreen = ({route}) => {
-  const {article} = route.params;
+export default ArticleScreen = ({ route }) => {
+  const { article } = route.params;
+
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }));
+        }}>
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: article }));
+        }}>
+        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: article.url }} />
     </SafeAreaView> // {styles.container}>
   );
