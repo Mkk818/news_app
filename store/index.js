f50314532ef01ea251ffc18ee834eee2c8763eb9
Mkk818 <miriks818@gmail.com>
2022-06-26 15:39:02 +0900

    保存忘れ

--- store/index.js
+++ store/index.js
@@ -1,7 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import userReducer from './reducers/user';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { persistor } from './store';
+import { persistReducer, persistStore } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
+
 const rootReducer = combineReducers({
   user: userReducer,
 });
