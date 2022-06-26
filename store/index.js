71735b1019bdcff8e010d4075a5a4d3a71b84b6b
Mkk818 <miriks818@gmail.com>
2022-06-26 15:35:59 +0900

    Reduxの永続化 ・アプリを再起動してもお気に入り機能の記録が残るようにする

--- store/index.js
+++ store/index.js
@@ -1,11 +1,20 @@
 import { createStore, combineReducers } from 'redux';
 import userReducer from './reducers/user';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
+import { persistor } from './store';
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'],
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
